dbe51834e4aee7d10bdf9a72fa1e82a6fca699e3
Lucas Gabriel Lima <lgl15@inf.ufpr.br>
2018-10-03 20:51:05 -0300

    TODO: contar tamanho areas, aí é só fazer um ranking das melhores e expandir elas

--- main.c
+++ main.c
@@ -2,28 +2,58 @@
 #include <stdlib.h>
 #include <graphviz/cgraph.h>
 
+typedef struct {
+  int nlinhas;
+  int ncolunas;
+  int ncores;
+  int **tabuleiro;
+} ttabuleiro;
+
 int main(int argc, char const *argv[]) {
   int i, j, n, m, k;
-  int **tabuleiro;
+  ttabuleiro t;
 
   //le lunhas e colinas
   scanf("%d %d %d\n", &n, &m, &k);
 
   //aloca tabuleiro
-  tabuleiro = (int **) malloc(n * sizeof(int));
-  for(int i = 0; i < n; i++) tabuleiro[i] = (int *) malloc(m * sizeof(int));
+  t.nlinhas = n;
+  t.ncolunas = m;
+  t.ncores = k;
+  t.tabuleiro = (int **) malloc(n * sizeof(int));
+  for(int i = 0; i < n; i++) t.tabuleiro[i] = (int *) malloc(m * sizeof(int));
 
-  //le tabuleiro
+  le tabuleiro
   for(i = 0; i < n; i++){
     for(j = 0; j < m; j++){
-      scanf("%d", &tabuleiro[i][j]);
+      scanf("%d", &t.tabuleiro[i][j]);
 
-      if(tabuleiro[i][j] > k){
-        printf("Cor %d fora do intervalo [1..%d]\n", tabuleiro[i][j], k);
+      if(t.tabuleiro[i][j] > k){
+        printf("Cor %d fora do intervalo [1..%d]\n", t.tabuleiro[i][j], k);
         exit(1);
       }
     }
   }
 
+  tamanho_area(t, 0, 0);
   return 0;
+}
+
+int valido(ttabuleiro t, int x, int y){
+  return (x >= 0) && (y >= 0) && (x < t.nlinhas) && (y < t.ncolunas);
+}
+
+int tamanho_area(ttabuleiro t, int x, int y){
+  char **visitados;
+  int cor = t.tabuleiro[x][y];
+
+  visitados = (char **) malloc(t.nlinhas * sizeof(char));
+  for(int i = 0; i < t.nlinhas; i++) visitados = (char *) malloc(t.ncolunas * sizeof(char));
+  // memset(visitados, 0, sizeof(visitados));
+
+  for(int i = 0; i < t.nlinhas;i++)
+    for(int j = 0; j < t.ncolunas; j++){
+
+
+    }
 }
